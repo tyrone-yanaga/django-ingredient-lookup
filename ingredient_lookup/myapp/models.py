67e8fccf4fdb391fb06fcfa138f3ec2be497456d
tyrone-yanaga <tkyanaga@gmail.com>
2024-08-12 00:28:30 -0700

    create item model & create db

--- ingredient_lookup/myapp/models.py
+++ ingredient_lookup/myapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    ingredientName = models.CharField(max_length=200)
+    pcode = models.IntegerField()
+    casNumber = models.CharField(max_length=50)
+    epaRegNumber = models.CharField(max_length=50)
+    productName = models.CharField(max_length=200)
+    productNameStatus = models.BooleanField()
+    redistrationStatus = models.BooleanField()
+    productStatusDate = models.DateField()
+    alternateBrandName = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.productName
