34d0c1f7122d5983a1044c4e998895b5b29f9bc0
tyrone-yanaga <tkyanaga@gmail.com>
2024-08-16 02:03:22 -0700

    remove magic numbers

--- ingredient_lookup/myapp/models.py
+++ ingredient_lookup/myapp/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
+MAX_NUM_LENGTH = 50
+MAX_NAME_LENGTH = 200  
 # Create your models here.
 class Item(models.Model):
-    ingredientName = models.CharField(max_length=200)
+    ingredientName = models.CharField(max_length=MAX_NAME_LENGTH)
     pcode = models.IntegerField()
-    casNumber = models.CharField(max_length=50)
-    epaRegNumber = models.CharField(max_length=50)
-    productName = models.CharField(max_length=200)
+    casNumber = models.CharField(max_length=MAX_NUM_LENGTH)
+    epaRegNumber = models.CharField(max_length=MAX_NUM_LENGTH)
+    productName = models.CharField(max_length=MAX_NAME_LENGTH)
     productNameStatus = models.BooleanField()
     redistrationStatus = models.BooleanField()
     productStatusDate = models.DateField()
-    alternateBrandName = models.CharField(max_length=200)
+    alternateBrandName = models.CharField(max_length=MAX_NAME_LENGTH)
 
     def __str__(self):
-        return self.productName
+        return f"{self.productName}"
