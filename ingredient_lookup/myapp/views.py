a0ac7efe9cda789037fc47d728cd00ff39e775d8
tyrone-yanaga <tkyanaga@gmail.com>
2024-08-16 03:12:06 -0700

    adding fetch data method

--- ingredient_lookup/myapp/views.py
+++ ingredient_lookup/myapp/views.py
@@ -1,3 +1,19 @@
+import requests
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from .forms import UserInputForm
 
-# Create your views here.
+def fetch_data(request):
+    data = None
+    if request.method == 'POST':
+        form = UserInputForm(request.POST)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            url = f'https://ordspub.epa.gov/ords/pesticides/cswu/ProductSearch/searchWithIngName/v1/{query}'
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+    else:
+        form = UserInputForm()
+
+    return render(request, 'myapp/fetch_data.html', {'form': form, 'data': data})
